6d7b73ce79053a4faaabb6b7fc67b15586924a2e
Daniel Boeve <dboeve@hearsaycorp.com>
2017-04-21 12:58:05 -0700

    Add alias symlinking to install

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s `pwd`/.vimrc ~/.vimrc
 vim +PlugInstall
-ln -s ~/projects/dotfiles/.gitconfig ~/.gitconfig
+ln -s `pwd`/.gitconfig ~/.gitconfig
+ln -s `pwd`/.aliases ~/.aliases
