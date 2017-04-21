eaf6171ba853f9fe79614168913885eb5d474c24
Daniel Boeve <dboeve@hearsaycorp.com>
2017-04-21 10:53:36 -0700

    Add gitconfig dotfile

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s `pwd`/.vimrc ~/.vimrc
 vim +PlugInstall
+ln -s ~/projects/dotfiles/.gitconfig ~/.gitconfig
