12614b188922cfcb35f704e025e17b683064733b
Daniel Boeve <dboeve@hearsaycorp.com>
2017-04-21 20:34:51 +0000

    Add .zshrc

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
+# vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s `pwd`/.vimrc ~/.vimrc
 vim +PlugInstall
+
+# git
 ln -s `pwd`/.gitconfig ~/.gitconfig
+
+# shell
+chsh /bin/zsh
 ln -s `pwd`/.aliases ~/.aliases
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+rm ~/.zshrc
+ln -s `pwd`/.zshrc ~/.zshrc
+
+# SSH
 ln -s `pwd`/.ssh/* ~/.ssh/
